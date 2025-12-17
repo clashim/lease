12856dc68d92fd2de37b9c632a1412b312bbf039
clashim <1017185043@qq.com>
2025-12-18 00:58:24 +0800

    一个sql语句还有点复杂的分页

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/ApartmentInfoService.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/ApartmentInfoService.java
@@ -16,4 +16,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface ApartmentInfoService extends IService<ApartmentInfo> {
 
+    void saveOrUpdateApartment(ApartmentSubmitVo apartmentSubmitVo);
+
+    IPage<ApartmentItemVo> pageApartmentItemByQuery(IPage<ApartmentItemVo> page, ApartmentQueryVo queryVo);
 }
